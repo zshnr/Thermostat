7c3fd47e3b072a8a396af98f2f2490e87233b9e8
Zeeshan Rasool <zrasool88@gmail.com>
2014-10-22 10:09:34 +0100

    temperature can increase in 1 degree increments, test passed

--- spec/thermoSpec.js
+++ spec/thermoSpec.js
@@ -1,18 +1,27 @@
+"use strict";
+
 describe('Thermostat', function(){
 
 	var thermostat;
 
-describe('Defaults', function(){
+describe('By Default', function(){
+
+	beforeEach(function(){
+		thermostat = new Thermostat();
+	});
 
 	it('Starts at 20 degrees', function(){
-		thermostat = new Thermostat();
 		expect(thermostat.temperature).toEqual(20);
 
 	});
 
 	it('power saving mode is on by default', function(){
-		thermostat = new Thermostat();
 		expect(thermostat.isPowerSaverOn).toBe(true);
+	});
+
+	it('can increase the temperature by one degree', function(){
+		thermostat.increaseTemp(1);
+		expect(thermostat.temperature).toEqual(21);
 	});
 
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -2,3 +2,7 @@ function Thermostat(){
 	this.temperature = 20;
 	this.isPowerSaverOn = true;
 };
+
+Thermostat.prototype.increaseTemp	 = function() {
+	this.temperature = this.temperature + 1;
+};
