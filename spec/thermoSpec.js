79556e8da5b78b53a350eb2655140ff8180b9957
Zeeshan Rasool <zrasool88@gmail.com>
2014-10-22 09:47:30 +0100

    initial commit

---
+++ spec/thermoSpec.js
@@ -0,0 +1,13 @@
+describe('Thermostat', function(){
+
+	var thermostat;
+
+describe('Defaults', function(){
+
+	it('Starts at 20 degrees', function(){
+		expect(thermostat.temperature).toEqual('20');
+
+	});
+
+});
+});
