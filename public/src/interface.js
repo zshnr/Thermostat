ac91e26f8bd44013bc8b6bf5d092aea41dbaf588
Camilla van Klinken <camillavk@gmail.com>
2014-10-23 13:56:31 +0100

    Two temperatures visible

--- public/src/interface.js
+++ public/src/interface.js
@@ -1,9 +1,9 @@
 function ThermostatView(element, currentTemp){
 	this.el = $(element);
 	this.thermostat = new Thermostat;
-	// this.el.text(this.thermostat.temperature);
+	this.el.text(this.thermostat.temperature);
 	// var currentTemp;
-	this.el.text(currentTemp);
+	// this.el.text(currentTemp);
 	this.bindTo('.increase_temperature', this.thermostat, this.thermostat.increaseTemp, this.el);
 	this.bindTo('.decrease_temperature', this.thermostat, this.thermostat.decreaseTemp, this.el);
 	this.bindTo('.reset', this.thermostat, this.thermostat.resetTemp, this.el);
